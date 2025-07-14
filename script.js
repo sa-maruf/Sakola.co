03d6592036400719676f5b4d2042216102112892
unknown <Sanowar-Alam-Maruf>
2025-07-14 15:28:26 +0600

    some commit

--- script.js
+++ script.js
@@ -1,14 +1,6 @@
-// Get references to the mobile menu button and the menu itself
-const mobileMenuButton = document.getElementById('mobile-menu-button');
-const mobileMenu = document.getElementById('mobile-menu');
-const hamburgerIcon = document.getElementById('hamburger-icon');
-const closeIcon = document.getElementById('close-icon');
+let mobMenuBtn = document.getElementById('mob-menu-btn');
+let mobMenu = document.getElementById('mob-menu')
 
-// Add a click event listener to the button
-mobileMenuButton.addEventListener('click', () => {
-    // Toggle the 'active' class on the mobile menu to show/hide it
-    mobileMenu.classList.toggle('active');
-    // Toggle the icons
-    hamburgerIcon.classList.toggle('hidden');
-    closeIcon.classList.toggle('hidden');
-});
+mobMenuBtn.addEventListener('click', () => {
+    mobMenu.classList.toggle('active')
+}) 
